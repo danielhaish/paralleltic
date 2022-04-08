99004cfc9a137b51fa847f0a3df3ff843d164d11
Jonatan <jonatan@localhost.localdomain>
2022-04-08 21:19:58 +0300

    asfasf

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from paralizethread import ParallelizeThread
 setup(name='split-threads',
-      version='1.0',
+      version='1.0.1',
       description='Python Distribution Utilities',
       author='Greg Ward',
       author_email='gward@python.net',
